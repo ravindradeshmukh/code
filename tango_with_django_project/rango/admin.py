0a528e88f788612e31cfcf9f008938a567ecb2ad
Ravindra Deshmukh <ravindra.deshmukh.sid@gmail.com>
2016-04-14 19:03:29 +0530

    date 14-4

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields={'slug':('name',)}
+
 
 class PageAdmin(admin.ModelAdmin):
 	#fields=['category','title','url','views','likes']
 	list_display=('title', 'category', 'url')
+
+admin.site.register(Category, CategoryAdmin)
 
 admin.site.register(Page, PageAdmin)
 
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
+from rango.models import Category
 
 def index(request):
-	context=RequestContext(request)
-	context_dict={'boldmessage': "I am bold font from the context"}
-	return render_to_response('rango/index.html', context_dict, context)
-	#response="Rango says Hello <a href='/rango/about/'>About</a>"
-	#return HttpResponse(response)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	context=RequestContext(request)	
