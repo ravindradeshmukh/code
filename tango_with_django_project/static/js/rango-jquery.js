f69221f834d98606da224ba5767c6a1d884f1729
Ravindra Deshmukh <ravindra.deshmukh.sid@gmail.com>
2016-04-21 19:33:04 +0530

    date 21-4

--- tango_with_django_project/static/js/rango-jquery.js
+++ tango_with_django_project/static/js/rango-jquery.js
@@ -2,6 +2,9 @@ $(document).ready( function() {
 
     $("#about-btn").click(function(event) {
         alert("You clicked the button using JQuery!");
-    
+    });
+    $("#about-btn2").click(function(event) {
+        alert("You clicked the button using JQuery!");
+   
     });
 });
